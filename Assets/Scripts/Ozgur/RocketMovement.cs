e38b306a6b67e6148c969296b443251be6821f61
0zeeE <ertekinozgur2000@hotmail.com>
2023-05-20 20:01:20 +0300

    Roket, Booster ve Level sistemi
    Roket kalibre edildi ve Booster eklendi. Ana haritadan alınıp kullanılabilmesi için DataHolder objesi ayarlandı ve içine kodlar eklendi

--- Assets/Scripts/Ozgur/RocketMovement.cs
+++ Assets/Scripts/Ozgur/RocketMovement.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class RocketMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int rocketLevel = 1;
+    public int fuelLevel = 1;
     public float flySpeed = 400f;
     public float rotateSpeed = 4f;
     public float fuelAmount = 50f;
     public float fuelDecreaseAmount = 2f;
-    public GameObject DataHolder;
+    public float maxHeight;
     Vector2 move;
     Transform transform;
     public Rigidbody2D rigidbody;
 
     void Start()
     {
+        maxHeight = 0;
         rigidbody = GetComponent<Rigidbody2D>();
         transform = GetComponent<Transform>();
-        DataHolder = GetComponent<GameObject>();
+
+        SetRocketLevel();
+        SetFuelLevel();
+        
     }
 
     // Update is called once per frame
@@ -30,6 +36,8 @@ public class RocketMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
+
         if(fuelAmount > 0)
         {
             if(move.y != 0)
@@ -42,11 +50,62 @@ public class RocketMovement : MonoBehaviour
                 rigidbody.AddForce(move * rotateSpeed * Time.deltaTime);
                 fuelAmount = fuelAmount - Time.deltaTime * fuelDecreaseAmount;
                 transform.Rotate(0, 0, -move.x);
-            }
-            
-            
-            
+            } 
         }
+
+        CalculateMaxHeight();
         
+    }
+
+    private void SetRocketLevel()
+    {
+        if (rocketLevel == 1)
+        {
+            flySpeed = 600f;
+            rotateSpeed = 4f;
+        }
+        else if (rocketLevel == 2)
+        {
+            flySpeed = 900f;
+            rotateSpeed = 5f;
+        }
+        else if (rocketLevel == 3)
+        {
+            flySpeed = 1000f;
+            rotateSpeed = 6f;
+        }
+        else
+        {
+            Debug.Log("Roket seviyesi belirlenmedi.");
+        }
+    }
+
+    private void SetFuelLevel()
+    {
+        if(fuelLevel == 1)
+        {
+            fuelAmount = 30f;
+        }
+        else if(fuelLevel == 2)
+        {
+            fuelAmount = 50f;
+        }
+        else if (fuelLevel == 3)
+        {
+            fuelAmount = 100f;
+        }
+        else
+        {
+            Debug.Log("Yakit seviyesi ayarlanmadi");
+        }
+    }
+
+    private void CalculateMaxHeight()
+    {
+        if(transform.position.y > maxHeight)
+        {
+            maxHeight = transform.position.y;
+        }
+        Debug.Log("Max Height: " + maxHeight);
     }
 }
