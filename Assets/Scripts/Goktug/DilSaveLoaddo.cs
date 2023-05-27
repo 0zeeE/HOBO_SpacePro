4c92eea0e69734a37f334b8d978c68d8d3b2a1e4
goktug5000 <goktug5000@gmail.com>
2023-05-28 02:59:53 +0300

    xml giriş

--- Assets/Scripts/Goktug/DilSaveLoaddo.cs
+++ Assets/Scripts/Goktug/DilSaveLoaddo.cs
@@ -6,34 +6,108 @@ using System.IO;
 public class DilSaveLoaddo : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    // Combine the data path with the desired file name
+    string filePathh;
+
     void Start()
     {
         // Get the path to the game's data folder
         string dataPath = Application.dataPath;
-
-        // Combine the data path with the desired file name
         string filePath = Path.Combine(dataPath, "Buildings.xml");
-
+        filePathh = filePath;
         // Check if the file already exists
         if (File.Exists(filePath))
         {
-            // Add text to the existing file
-            File.AppendAllText(filePath, "New line added!");
+
+            File.AppendAllText(filePath, ("<!--"+ "BURAYA BAKARLAR" +"-->"));
 
             Debug.Log("Text added to the file at: " + filePath);
         }
         else
         {
             // Create a new file and write text
-            File.WriteAllText(filePath, "Hello, world!");
+            File.WriteAllText(filePath, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 
             Debug.Log("Text file created at: " + filePath);
         }
     }
+    public void saveGame()
+    {
+        if (File.Exists(filePathh))
+        {
 
+            try
+            {
+                File.Delete(filePathh);
+                Debug.Log("File deleted successfully.");
+            }
+            catch (IOException e)
+            {
+                Debug.Log("An error occurred while deleting the file: " + e.Message);
+            }
+            saveGame();
+        }
+        else
+        {
+            // Create a new file and write text
+            File.WriteAllText(filePathh, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+
+            Debug.Log("Text file created at: " + filePathh);
+        }
+
+        File.AppendAllText(filePathh, "<All>");
+
+        Inventory[] Depomuz = GameObject.FindObjectsOfType<Inventory>();
+        foreach(Inventory depo in Depomuz)
+        {
+            if (depo != null)
+            {
+                File.AppendAllText(filePathh, "<depo>");
+                foreach (myMaterialHolder myMatHolder in depo.sahiplerim)
+                {
+                    if (myMatHolder.myMateriall != null)
+                    {
+                        File.AppendAllText(filePathh, "<item>");
+
+                        File.AppendAllText(filePathh, "<matName>");
+                        File.AppendAllText(filePathh, myMatHolder.myMateriall.name);
+                        File.AppendAllText(filePathh, "</matName>");
+                        File.AppendAllText(filePathh, "<amountt>");
+                        File.AppendAllText(filePathh, myMatHolder.amountt.ToString());
+                        File.AppendAllText(filePathh, "</amountt>");
+
+                        File.AppendAllText(filePathh, "</item>");
+                    }
+
+                }
+                File.AppendAllText(filePathh, "</depo>");
+            }
+            
+        }
+
+        Building[] builds = GameObject.FindObjectsOfType<Building>();
+        foreach (Building bui in builds)
+        {
+            if (bui != null)
+            {
+                File.AppendAllText(filePathh, "<building>");
+                File.AppendAllText(filePathh, ("<building_name>" + bui.gameObject.name + "</building_name>"));
+                File.AppendAllText(filePathh, ("<building_exp>" + bui.Exp + "</building_exp>"));
+
+                File.AppendAllText(filePathh, "</building>");
+            }
+        }
+
+        File.AppendAllText(filePathh, "</All>");
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            saveGame();
+        }
     }
 }
